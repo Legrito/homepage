2e917710f25e6ad144dc05cbab88ef8e61a0d20a
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2021-05-03 10:50:06 +0300

    Add princess theme elements & styles

--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@
     const unicornEl = document.querySelector('.unicorn-wrap');
     const crownEl = document.querySelector('.crown');
     const magicWandEl = document.querySelector('.magic-wand');
+    const flowersEl = document.querySelector('.flowers')
 
 
     const onChangeTheme = (e) => {
         unicornEl.classList.toggle('js-show-unicorn');
         crownEl.classList.toggle('js-show');
         magicWandEl.classList.toggle('js-show');
+        flowersEl.classList.toggle('js-show');
         
     }
 
